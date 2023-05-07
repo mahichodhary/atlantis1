d14c012a99e81894650f9ef55a96b0e88eefc9f5
Mahima Chaudhary <130489002+mahichodhary@users.noreply.github.com>
2023-05-07 21:33:19 +0530

    Update main.tf

--- production/EC2/main.tf
+++ production/EC2/main.tf
@@ -3,3 +3,7 @@ resource "aws_instance" "atlantis_ec2" {
   ami = "ami-02396cdd13e9a1257"
   instance_type = "t2.micro"
 }
+resource "aws_instance" "atlantis_ec3" {
+  ami = "ami-02096cdd13e9a1257"
+  instance_type = "t2.micro"
+}
