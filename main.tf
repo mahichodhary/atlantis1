c5ccc3d2a21184dfed030e73b57ec90fa003b3d3
Mahima Chaudhary <130489002+mahichodhary@users.noreply.github.com>
2023-05-08 09:54:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,7 @@ resource "null_resource" "example" {
 }
 resource "null_resource" "test" {
 }
-
+resource "aws_instance" "ec2-sever" {
+  ami = "ami-02396cdd13e9a1257"
+  instance_type = "t2.micro"
+}
