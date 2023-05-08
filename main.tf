d1a3f8c9c2e5b2bc9e66b03fe4a910b69c64cc89
Mahima Chaudhary <130489002+mahichodhary@users.noreply.github.com>
2023-05-08 12:55:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,7 @@ resource "null_resource" "test" {
 
 resource "null_resource" "test9899" {
 }
+resource "aws_instance" "atlakjkntis_ec2" {
+  ami = "ami-02396cdd13e9a1257"
+  instance_type = "t2.micro"
+}
