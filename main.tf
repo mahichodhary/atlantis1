009e9a627bf641148d3211a15646e812895a3afc
Mahima Chaudhary <130489002+mahichodhary@users.noreply.github.com>
2023-05-10 10:30:25 +0530

    Update main-repo

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-resource "aws_instance" "atlantis_ec2" {
-  ami = "ami-02396cdd13e9a1257"
-  instance_type = "t2.micro"
-}
+
 resource "null_resource" "example" {
 }
 resource "null_resource" "test" {
