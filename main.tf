dbbcf824f4bd057340b2947e9ea330e1a76e4e6c
Mahima Chaudhary <130489002+mahichodhary@users.noreply.github.com>
2023-05-10 10:20:22 +0530

    Update mainofprod

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-resource "aws_instance" "atlantis_ec2" {
-  ami = "ami-02396cdd13e9a1257"
-  instance_type = "t2.micro"
-}
+
 resource "null_resource" "example" {
 }
-resource "null_resource" "test" {
+resource "null_resource" "tes77t" {
 }
 
+resource "null_resource" "examp77le" {
+}
+resource "null_resource" "test8" {
+}
